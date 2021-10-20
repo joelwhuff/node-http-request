db8f9449a46982153d8a40d52da2dfb107a51a03
Joel <joelhuff.dev@gmail.com>
2021-10-20 01:51:34 -0400

    added redirect for 301 and 308 status codes

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-import HTTP from "./HTTP.js";
+import request from "./request.js";
 
-Promise.all([
-  HTTP.get("http://canvasrider.com"),
-  HTTP.get("http://canvasrider.com"),
-  HTTP.get("http://canvasrider.com/tracks/1"),
-  HTTP.get("http://canvasrider.com"),
-])
-  .then((data) => {
-    console.log(data[2]);
-  })
-  .catch(console.error);
+export default request;
---
+++ request.js
@@ -0,0 +1,62 @@
+import http from "http";
+import https from "https";
+
+const UTF8 = /text|xml|json|ecmascript/;
+
+/**
+ * Promise based HTTP request helper. Converts response data
+ * to utf-8 depending on the HTTP Content-Type
+ *
+ * @param {string} [url]
+ * @param {Object} options - node http request options. Use options.data to write data to post requests
+ * @returns {Promise} - returns response data on success
+ */
+function request(url, options = {}) {
+  return new Promise((resolve, reject) => {
+    if (typeof url === "string") {
+      try {
+        let urlObject = new URL(url);
+
+        options.host = urlObject.host;
+        options.path = urlObject.pathname;
+        urlObject.port && (options.port = urlObject.port);
+        options.protocol = urlObject.protocol;
+      } catch (err) {
+        reject(err);
+      }
+    } else {
+      Object.assign(options, url);
+    }
+
+    let client = options.protocol === "https:" ? https : http;
+
+    let req = client.request(options, (res) => {
+      if (res.statusCode === 301 || res.statusCode === 308) {
+        resolve(request(res.headers.location, options));
+      }
+
+      let chunks = [];
+
+      res.on("data", (chunk) => {
+        chunks.push(chunk);
+      });
+
+      res.on("end", () => {
+        let data = Buffer.concat(chunks);
+        resolve(UTF8.test(res.headers["content-type"]) ? data.toString("utf8") : data);
+      });
+
+      res.on("error", reject);
+    });
+
+    req.on("error", reject);
+
+    if (options.data) {
+      req.write(options.data);
+    }
+
+    req.end();
+  });
+}
+
+export default request;
