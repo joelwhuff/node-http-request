1c9d05e49583ee09c269ec90c63af4fa2c162c10
Joel <joelhuff.dev@gmail.com>
2022-04-06 22:45:08 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
 import http from 'http';
 import https from 'https';
-
-// todo - add support for accept-encoding
-
-const UTF8 = /(text|json|xml|javascript)/;
-const REDIRECT = [301, 302, 307, 308];
+import { finished } from 'stream';
 
 /**
- * Promise based HTTP request helper
- * @param {(string|URL)} url
- * @param {Object} [options] Node http request options https://nodejs.org/api/http.html#httprequesturl-options-callback
- * @param {(string|Buffer)} [options.body] Data to write to POST requests
- * @returns {Promise} Response
+ * @param {string} url
+ * @param {Object} [options]
+ * @param {string} [options.method=GET]
+ * @param {(string|Buffer)} [options.postData]
  */
 const request = (url, options = {}) => {
     return new Promise((resolve, reject) => {
-        let _url = url;
-        if (typeof url === 'string') {
-            try {
-                _url = new URL(url);
-            } catch (err) {
-                reject(err);
-            }
+        let _url;
+        try {
+            _url = new URL(url);
+        } catch (err) {
+            reject(err);
+            return;
         }
 
         let client = _url.protocol === 'https:' ? https : http;
 
         let req = client.request(_url, options, res => {
-            if (REDIRECT.includes(res.statusCode)) {
-                resolve(request(res.headers.location, options));
-            }
-
             let chunks = [];
 
-            res.on('error', reject)
-                .on('data', chunk => {
-                    chunks.push(chunk);
-                })
-                .on('end', () => {
-                    let data = Buffer.concat(chunks);
-                    resolve({
-                        httpVersion: res.httpVersion,
-                        statusCode: res.statusCode,
-                        statusMessage: res.statusMessage,
-                        headers: res.headers,
-                        data: UTF8.test(res.headers['content-type']) ? data.toString() : data,
-                    });
+            finished(res, err => {
+                if (err) reject(err);
+            });
+
+            res.on('data', chunk => {
+                chunks.push(chunk);
+            });
+
+            res.on('end', () => {
+                let data = Buffer.concat(chunks);
+
+                resolve({
+                    httpVersion: res.httpVersion,
+                    statusCode: res.statusCode,
+                    statusMessage: res.statusMessage,
+                    headers: res.headers,
+                    data: /text|json|javascript/.test(res.headers['content-type']) ? data.toString() : data,
                 });
+            });
         });
 
-        req.on('error', reject);
+        finished(req, err => {
+            if (err) reject(err);
+        });
 
-        if (options.method?.toLowerCase() === 'post' && options.body) {
+        if (options.method?.toUpperCase() === 'POST' && options.postData) {
             req.write(options.body);
         }
 
